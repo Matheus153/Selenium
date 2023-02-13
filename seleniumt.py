78444bf6d0e7d9f7efa77630d2a43d6c4885bcaa
Matheus <santiosmatheus@gmail.com>
2023-02-12 23:02:35 -0400

    first

---
+++ seleniumt.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+import time
+
+options = Options()
+options.add_argument("--headless")
+
+servico = Service(GeckoDriverManager().install())
+
+navegador = webdriver.Firefox(service=servico, options=options)
+
+navegador.get('https://select-quiz.vercel.app')
+
+navegador.find_element(
+    'xpath', '/html/body/div/div/div[1]/div/a[5]').click()
+
+time.sleep(2)
+
+navegador.find_element(
+    'xpath', '/html/body/div/div/div[1]/section/div/form/div/input').send_keys('Pseudomusk')
+
+time.sleep(2)
+
+navegador.find_element(
+    'xpath', '/html/body/div/div/div[1]/section/div/form/button').click()
+
+navegador.quit()
